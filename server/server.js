d8baa8f7c0b2e88916b19527aaee3302cb7777dc
Sukhrahmat <233301627+rahmat7sq@users.noreply.github.com>
2026-08-30 10:05:26 +0530

    app.get moved after app express

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const express = require ('express');
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
+
 const mongoose = require ('mongoose');
 const cookieParser = require ('cookie-parser')
 const cors = require('cors');
@@ -25,6 +23,11 @@ mongoose.connect(process.env.MONGO_URL)
 
 const app = express()
 const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
 
 
 app.use(
