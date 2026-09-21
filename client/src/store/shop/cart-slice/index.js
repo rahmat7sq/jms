f561b3dba6ff83fa94fbd74cae1264e1428e5705
Sukhrahmat <233301627+rahmat7sq@users.noreply.github.com>
2026-09-21 22:09:22 +0530

    Reset cart on logout

--- client/src/store/shop/cart-slice/index.js
+++ client/src/store/shop/cart-slice/index.js
@@ -63,7 +63,11 @@ export const updateCartQuantity = createAsyncThunk(
 const shoppingCartSlice = createSlice({
   name: "shoppingCart",
   initialState,
-  reducers: {},
+  reducers: {
+    resetCart: (state) => {
+      state.cartItems = [];
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(addToCart.pending, (state) => {
